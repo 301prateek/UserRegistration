40398b015ea7d22d31ca47e323fd5f2a7ca75883
301prateek <prateek.patil301@gmail.com>
2020-11-19 15:07:49 +0530

    Added: Last Name Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -22,12 +22,31 @@ public class UserRegistration{
 		}
 	}
 
+	public void validateLastName() {
+		System.out.println("Enter Your Last Name : ");
+		Scanner scanner = new Scanner(System.in);
+		String lastName = scanner.nextLine();
 
+		String expression = "^[A-Z][a-z]{2,}$";
+		int invalid = 0;
+
+		if(lastName.matches(expression)) {
+			System.out.println("Accepted Valid Last Name");
+		}
+		else {
+			System.out.println("First letter Should be capital, minimum 3 characters");
+			invalid = 1;
+		}
+		if(invalid == 1) {
+			validateLastName();
+		}
+	}
 
 	public static void main(String[] args){
 
 		UserRegistration uservalidation = new UserRegistration();
 		uservalidation.validateFirstName();
+		uservalidation.validateLastName();
 
 	}
 }
